7805c0cea09b887ec1d07fc5c1a2b93782a6e9ae
garam9koo <garam9koo@gmail.com>
2024-10-04 18:00:48 +0900

    draggables drop available

--- src/dragndrop.js
+++ src/dragndrop.js
@@ -1,30 +1,24 @@
-const cards = document.querySelectorAll('.card.node');
-const board = document.querySelector('.board');
+const cards = document.querySelectorAll('.node');
+const board = document.querySelector('#board');
 
-// cards.forEach((card, index) => {
-//     card.addEventListener('dragstart', e=>{
-//         e.dataTransfer.setData('text/plain', card.id);
-//         e.target.style.opacity = '0.5';
-//     });
-//     card.addEventListener('dragend',(e)=>{
-//         e.target.style.opacity = '';
-//     });
-// });
+cards.forEach((card) => {
+    card.addEventListener('dragstart', e=>{
+        e.dataTransfer.setData('text/plain', card.id);
+        e.target.style.opacity = '0.5';
+    });
+    card.addEventListener('dragend',(e)=>{
+        e.target.style.opacity = '';
+    });
+});
 
-// board.addEventListener('dragover', e=>{
-//     e.preventDefault();
-// });
+board.addEventListener('dragover', e=>{
+    e.preventDefault();
+});
 
-// board.addEventListener('drop', e =>{
-//     e.preventDefault();
-//     const cardId = e.dataTransfer.getData('text/plain');
-//     const draggedCard = document.getElementById(cardId);
-//     const clone = draggedCard.cloneNode(true);
-
-//     clone.id = '${cardId}-copy';
-//     clone.style.opacity = '';
-//     board.appendChild(clone);
-// })
+board.addEventListener('drop', e =>{
+    e.preventDefault();
+    const cardId = e.dataTransfer.getData('text/plain');
+})
 
 const cy = cytoscape({
     container: document.getElementById('board'),
